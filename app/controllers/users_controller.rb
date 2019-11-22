af31f59ad0612a32b14e4cb1d5cdaa06606422bc
Michael Chen <michael.chen.1120@gmail.com>
2019-11-22 15:24:56 -0500

    Add users and sessions controllers. Add new and create methods for users controller

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,24 @@
+class UsersController < ApplicationController
+
+    def new
+        render :new
+    end
+
+    def create
+        @user = User.new(user_params)
+
+        if @user.save
+            login!(@user)
+            redirect_to root_url
+        else
+            render json: @user.errors.full_messages
+        end
+    end
+
+    private
+
+    def user_params
+        params.require(:user).permit(:email, :password)
+    end
+
+end
