4cb56c47aaef8dd97941933f2778c71711cd64f6
Michael Chen <michael.chen.1120@gmail.com>
2019-11-23 14:09:27 -0500

    Built views for all actions for bands controller.

--- app/controllers/bands_controller.rb
+++ app/controllers/bands_controller.rb
@@ -11,12 +11,12 @@ class BandsController < ApplicationController
     end
 
     def create
-        @band = Band.new
+        @band = Band.new(band_params)
 
         if @band.save
             redirect_to band_url(@band)
         else
-            flash.now[:errors] = @band.errors.full_messages
+            flash[:errors] = @band.errors.full_messages
             render :new
         end
     end
@@ -45,6 +45,7 @@ class BandsController < ApplicationController
     def destroy
         @band = Band.find(params[:id])
         @band.destroy
+        redirect_to bands_url
     end
 
     private
