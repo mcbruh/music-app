4f10c2b810d636dab3c96a46ccbdd55b324c91e9
Michael Chen <michael.chen.1120@gmail.com>
2019-11-22 21:47:04 -0500

    Add bands migration and model with name validations. Add routes for bands

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :users, only: [:new, :create, :show]
   resource :session
+  resources :bands, only: [:index, :new, :create, :show, :edit, :update, :destroy]
 end
