3f120a950c950f28e628c5b2d350ff2888d5a8a3
Michael Chen <michael.chen.1120@gmail.com>
2019-11-23 19:16:24 -0500

    Fix validations for albums and add default for live. Adding form for albums

--- app/models/album.rb
+++ app/models/album.rb
@@ -1,6 +1,11 @@
 class Album < ApplicationRecord
 
-    validates :title, :band_id, :year, :live, presence: true
+    validates :title, :band_id, :year, presence: true
+    validates :live, inclusion: { in: [true, false] }
     belongs_to :band
+    after_initialize :set_defaults
 
+    def set_defaults
+        self.live ||= false
+    end
 end
