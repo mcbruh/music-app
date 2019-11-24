e33482f03f570bd570b86efffe500132532a15d9
Michael Chen <michael.chen.1120@gmail.com>
2019-11-24 13:43:21 -0500

    Add albums show page with edit and delete buttons. Fix albums form for band_id. Add show action for albums controller.

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -16,13 +16,23 @@ class AlbumsController < ApplicationController
         end
     end
 
+    def show
+        @album = Album.find_by(id: params[:id])
+        if @album.nil?
+            redirect_to bands_url
+        else
+
+            render :show
+        end
+    end
+
     def edit
-        @album = Album.find(params[:id])
+        @album = Album.find_by(id: params[:id])
         render :edit
     end
 
     def update
-        @album = Album.find(params[:id])
+        @album = Album.find_by(id: params[:id])
 
         if @album.update_attributes(album_params)
             redirect_to album_url(@album)
@@ -33,15 +43,15 @@ class AlbumsController < ApplicationController
     end
 
     def destroy
-        @album = Album.find(params[:id])
+        @album = Album.find_by(id: params[:id])
         @album.destroy
-        redirect_to albums_url
+        redirect_to bands_url
     end
 
     private
 
     def album_params
-        params.require(:album).permit(:title, :band_id, :year, :live)
+        params.require(:album).permit(:title, :year, :live, :band_id)
     end
 
 end
