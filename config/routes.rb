f2645ef4d90f7ad6e18f9bc3d8a00451901774d0
Michael Chen <michael.chen.1120@gmail.com>
2019-11-24 16:23:32 -0500

    Add routes for tracks

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,10 @@ Rails.application.routes.draw do
   resources :users, only: [:new, :create, :show]
   resource :session
   resources :bands, only: [:index, :new, :create, :show, :edit, :update, :destroy] do
-    resources :albums, only: :new
+    resources :albums, only: [:new]
   end
-  resources :albums, only: [:create, :show, :edit, :update, :destroy]
+  resources :albums, only: [:create, :show, :edit, :update, :destroy] do
+    resources :tracks, only: [:new]
+  end
+  resources :tracks, only: [:create, :show, :edit, :update, :destroy]
 end
