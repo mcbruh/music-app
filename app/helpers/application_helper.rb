0ad7aebcc5e5ca0e743ec1b555fcc771bec2d14f
Michael Chen <michael.chen.1120@gmail.com>
2019-11-24 21:13:09 -0500

    Add ugly_lyrics helper method. Use ugly_lyrics helper method on tracks show page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+
+    def ugly_lyrics(lyrics)
+        pretty_lyrics = ""
+        lyrics.each_line do |line|
+            pretty_lyrics << "&#9835; #{html_escape(line)}"
+        end
+        "<pre>#{pretty_lyrics}</pre>".html_safe
+    end
 end
